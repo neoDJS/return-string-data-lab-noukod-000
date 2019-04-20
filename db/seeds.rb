c5b7289924716093f4d819df1883b8c06c0212bb
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-20 18:13:57 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-
+descriptions = ["you are a tv.. and you held so much show tv to make life happier.", "VCR is past technology, but he got some great years.", "macbook is still at is best days, and kepp improve the technology scene."]
+Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.")
 10.times do
   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+  Product.create(:name => products.sample, :description => descriptions.sample, :price => Faker::Number.between(1,1500))
 end
 5.times do
   Customer.create(:name => Faker::Name.name)
@@ -22,4 +23,3 @@ end
   order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
   order.products << Product.find(Product.all.collect(&:id).sample)
 end
-
