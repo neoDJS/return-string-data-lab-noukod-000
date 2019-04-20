2a7365ea1fefff91ffc712de56679e400a2e52f1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-20 15:49:27 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,8 @@ class ProductsController < ApplicationController
   end
 
   def body
-    post = Post.find(params[:id])
-    render plain: post.description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
 end
